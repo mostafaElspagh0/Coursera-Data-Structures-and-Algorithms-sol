7ab591db70c3be35bc9a37134dfceb45d97dfe21
mostafa elspagh <51961781+mostafaElspagh0@users.noreply.github.com>
2020-07-03 08:56:11 +0200

    Create Last Digit of a Large Fibonacci Number.cpp

---
+++ Algorithmic Toolbox/week 2/Last Digit of a Large Fibonacci Number/Last Digit of a Large Fibonacci Number.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long a=1, b=0,c, n;
+    cin>>n;
+    for(int i = 0 ; i < n ; i++){
+        c = a;
+        a = b ;
+        b += c;
+        a %= 10;
+        b %= 10;
+    }
+    cout<<b<<endl;
+}
